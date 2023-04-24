5da702faac2aa06bce38bdd3aa12ed6248830617
Shivam Yadav <yadavshivam24012002@gmail.com>
2023-04-24 22:26:44 +0530

    order api continued

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,101 @@
+const Order = require('../models/orderModel');
+const Product = require('../models/productModel');
+const ErrorHandler = require('../utils/errorHandler');
+const catchAsyncErrors = require('../middleWare/catchAsyncErrors');
+
+
+
+// create new order
+exports.newOrder = catchAsyncErrors(async(req,res,next)=>{
+    const {shippingInfo,orderItems,paymentInfo,itemsPrice,taxPrice,shippingPrice,totalPrice} = req.body
+
+    const order = await Order.create({
+        shippingInfo,
+        orderItems,
+        paymentInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paidAt: Date.now(),
+        user:req.user._id, 
+    });
+
+    res.status(201).json({
+        success:true,
+        order,
+    }); 
+
+
+});
+
+
+// get single order
+exports.getSingleOrder = catchAsyncErrors(async(req,res,next)=>{
+    const order = await Order.findById(req.params.id).populate('user','name email');
+
+    if (!order) {
+        return next(new ErrorHandler(`Order not found !! `,404));
+    }
+
+    res.status(200).json({
+        success:true,
+        order,
+    })
+})
+
+
+
+// get logged in user orders
+exports.myOrders = catchAsyncErrors(async(req,res,next)=>{
+    const orders = await Order.find({user: req.user._id});
+
+    res.status(200).json({
+        success:true,
+        orders,
+    });
+});
+
+
+
+// get all orders -- ADMIN
+exports.getAllOrders = catchAsyncErrors(async(req,res,next)=>{
+    const orders = await Order.find();
+
+    let totalAmount = 0;
+
+    orders.forEach(order=>{
+        totalAmount += order.totalPrice;
+    });
+
+
+
+    res.status(200).json({
+        success:true,
+        totalAmount,
+        orders,
+    });
+});
+
+
+// update order status -- ADMIN
+exports.updateOrder = catchAsyncErrors(async(req,res,next)=>{
+
+    const order = await Order.find();
+
+    if (order.orderStatus === 'Delivered') {
+        return next(new ErrorHandler(`You have already delivered this order.`,400))
+    }
+
+    order.orderItems.forEach(order=>{
+        // await updateStock(order.Product,order.quantity);
+    });
+
+    order.orderStatus = req.body.status;
+    order.deliveredAt = Date.now()
+
+    res.status(200).json({
+        success:true,
+    });
+
+});
